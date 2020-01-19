6a25f9b47408b8c40c306d96029651ee50eb5444
Sebastian <sebastian.lauks13@gmail.com>
2020-01-19 17:43:43 +0100

    added custom edit text and custom action bar

--- app/src/main/java/com/example/po_dodawanietrasy/view/MainActivity.java
+++ app/src/main/java/com/example/po_dodawanietrasy/view/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.po_dodawanietrasy.view;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -37,6 +39,7 @@ public class MainActivity extends AppCompatActivity implements DodawanieTrasyVie
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar)findViewById(R.id.activity_main_toolbar));
 
         presenter = new DodawanieTrasyPresenter(this);
 
